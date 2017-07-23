6640e45db4817a60878ceb7886cc5bc1ace197c2
Adam Sturge <AdamSturge991@hotmail.com>
2017-07-23 10:16:34 -0400

    removed drag

--- docs/search/functions_3.js
+++ docs/search/functions_3.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['enabledrag',['EnableDrag',['../classNetForceAccumulator.html#acb0b119c5f4793003d045c9fd77a64e2',1,'NetForceAccumulator']]],
   ['enablegravity',['EnableGravity',['../classNetForceAccumulator.html#a92f06c86781e56e364596bc44b427a08',1,'NetForceAccumulator']]],
   ['expliciteuler',['ExplicitEuler',['../classExplicitEuler.html#aa16df6a77bb175fdd6291c302cb61d17',1,'ExplicitEuler::ExplicitEuler()'],['../classExplicitEuler.html#ab7ba8cf17d34831178c47a3a3711c3b0',1,'ExplicitEuler::ExplicitEuler(GLfloat dt)']]]
 ];
--- docs/search/functions_a.js
+++ docs/search/functions_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['scene',['Scene',['../classScene.html#ad10176d75a9cc0da56626f682d083507',1,'Scene::Scene()'],['../classScene.html#a8040db7addb6ae23254bd517323b65fa',1,'Scene::Scene(std::shared_ptr&lt; TimeIntegrator &gt; integrator, NetForceAccumulator net_force_accumulator)']]],
-  ['setdragcoeff',['SetDragCoeff',['../classDragForceGenerator.html#a932f6198843366df598b793ee8531264',1,'DragForceGenerator::SetDragCoeff()'],['../classNetForceAccumulator.html#a4bb41addcdf120d4b163a75ff36b2358',1,'NetForceAccumulator::SetDragCoeff()']]],
+  ['setdragcoeff',['SetDragCoeff',['../classDragForceGenerator.html#a932f6198843366df598b793ee8531264',1,'DragForceGenerator']]],
   ['setmodelmatrix',['SetModelMatrix',['../classModel.html#a6b79c9e0c9abc50896e70fe1fabff4e7',1,'Model']]],
   ['setnextposition',['SetNextPosition',['../classPhysicsEntity.html#a18913bb368bbe6e61e6838d7e8353bd4',1,'PhysicsEntity']]],
   ['setnextvelocity',['SetNextVelocity',['../classPhysicsEntity.html#a6d23d47421d535cc934405e8a7d1a43b',1,'PhysicsEntity']]],
